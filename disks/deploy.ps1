c0c2d95b25e0ba572d0ab917de14adc26565e845
satak <sami.koskivaara@gmail.com>
2019-01-28 20:40:03 +0200

    Add CircleCI

--- disks/deploy.ps1
+++ disks/deploy.ps1
@@ -8,5 +8,5 @@ if(!$token) {
     Write-Error "Token environment variable missing!"
 } else {
     Write-Output "Deploying cloud function to GCP project: $Project"
-    gcloud beta functions deploy disks --entry-point main --runtime python37 --trigger-http --project "$Project" --region europe-west1 --set-env-vars TOKEN="$token" --source ./src
+    gcloud beta functions deploy snapshot --entry-point main --runtime python37 --trigger-http --project "$Project" --region europe-west1 --set-env-vars TOKEN="$token" --source ./src
 }
